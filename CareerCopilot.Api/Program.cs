7fff9699c740fbdb72f4f2c7d11e7c92e2f4e6fc
José Emanuel RG <resendizgonzalezjoseemanuel@gmail.com>
2026-03-26 09:26:39 -0600

    Sacar environment de if

--- CareerCopilot.Api/Program.cs
+++ CareerCopilot.Api/Program.cs
@@ -41,12 +41,15 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Career Copilot API v1");
+    c.RoutePrefix = string.Empty; 
+});
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHangfireDashboard(); //Habilitar Dashboard, ver interfaz grafica de las tareas en /hangfire
+    app.UseHangfireDashboard();
 }
 
 app.UseHttpsRedirection();
